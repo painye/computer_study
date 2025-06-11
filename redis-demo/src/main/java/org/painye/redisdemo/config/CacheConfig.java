9a86a24ab655c67e28d0d6efe0049a2c5999d137
painye <1850384745@qq.com>
2025-06-11 09:30:11 +0800

    feat(redis): 初始化在redis上包裹一层本地类

---
+++ redis-demo/src/main/java/org/painye/redisdemo/config/CacheConfig.java
@@ -0,0 +1,12 @@
+package org.painye.redisdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author painye
+ * @Description
+ * @create 2025-06-10 16:11
+ */
+@Configuration
+public class CacheConfig {
+}
