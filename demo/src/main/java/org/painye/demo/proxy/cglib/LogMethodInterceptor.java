a889fb4030eb2099503093970ca31cb1e6b8b88c
painye <1850384745@qq.com>
2025-06-22 11:22:34 +0800

    feat(代理): 使用cglib继承的思想实现动态代理

---
+++ demo/src/main/java/org/painye/demo/proxy/cglib/LogMethodInterceptor.java
@@ -0,0 +1,34 @@
+package org.painye.demo.proxy.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author painye
+ * @Description
+ * @create 2025-06-22 10:38
+ */
+public class LogMethodInterceptor implements MethodInterceptor {
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        System.out.println("===>开始执行");
+        // 注意这里一点过要用MethodProxy.invokeSuper进行方法调用，不能用原方法，否则会有问题
+        Object result = proxy.invokeSuper(obj, args);
+        System.out.println("===>执行结束");
+        return result;
+    }
+
+
+
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Service.class);
+        enhancer.setCallback(new LogMethodInterceptor());
+        Service proxyService = (Service) enhancer.create();
+        proxyService.doService();
+    }
+}
---
+++ demo/src/main/java/org/painye/demo/proxy/cglib/Service.java
@@ -0,0 +1,14 @@
+package org.painye.demo.proxy.cglib;
+
+/**
+ * @author painye
+ * @Description 服务类
+ * @create 2025-06-22 10:35
+ */
+public class Service {
+
+    public void doService() {
+        System.out.println("执行服务");
+        //return "OK";
+    }
+}
